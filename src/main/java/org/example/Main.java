6dbb07377dc7ccd23dd7a4bfed3ee249b1d80196
Юрий <doribidontov@sfedu.ru>
2023-12-05 00:45:11 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,80 +1,65 @@
 package org.example;
 
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    //Функкция на вход идёт массив из чётного количества целых чисел
-    //Функция возвращает минимизированную максимальную сумму пар после оптимального объединения элементов в пары
-    public static int minPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int output = 0;
-        for(int i = 0; i < nums.length; i++){
-            output = Math.max(output, nums[i]+nums[nums.length-1-i]);
-        }
-        return output;
-    }
-    //Функция на вход принимает двумерный массив чисел
-    //На выход идёт массив чисел, пройденных по диагонали
-    public static int[] findDiagonalOrder(List<List<Integer>> nums) {
-        List<Integer> out = new ArrayList<>();
-        if (nums.size() == 1) {
-            out.addAll(nums.get(0));
-        } else {
-            int max = 0;
-            int x, y = 0;
-            for (x = 0; x < nums.size(); x++) {
-                int a = x, b = y;
-                if (max < nums.get(a).size()) max = nums.get(a).size();
-                while (a >= 0) {
-                    if (nums.get(a).size() - 1 >= b)
-                        out.add(nums.get(a).get(b));
-                    a--;
-                    b++;
-                }
-            }
-            x--;
-            for (y = 1; y < max; y++) {
-                int a = x, b = y;
-                while ((b < max)&&(a>=0)) {
-                    if (nums.get(a).size() - 1 >= b)
-                        out.add(nums.get(a).get(b));
-                    a--;
-                    b++;
-                }
-            }
-        }
-        int[] output = new int[out.size()];
-        for (int i = 0; i < out.size(); i++) output[i] = out.get(i);
-        return output;
-    }
+    static final File file = new File("src/main/java/org/example/text/input.txt");
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Choose your task \n1. Min Pair Sum. \n2. Find Diagonal Order");
-        int a = in.nextInt();
-        in.nextLine();
-        int n = in.nextInt();
-        in.nextLine();
-        switch (a){
-            case 1 ->{
-                int[] arr = new int[n];
-                for(int i = 0; i < n; i++) arr[i] = in.nextInt();
-                System.out.println(minPairSum(arr));
-            }
-            case 2 ->{
-                List<List<Integer>> list = new ArrayList<>();
-                String str;
-                for (int j = 0; j < n; j++) {
-                    str = in.nextLine();
-                    String[] s = str.split(" ");
-                    int[] arr = new int[s.length];
-                    for (int i = 0; i < s.length; i++) arr[i] = Integer.parseInt(s[i]);
-                    list.add(Arrays.stream(arr).boxed().toList());
+    public static int painter(boolean[][] holst, int[][] coords,int n){
+        int output = 0;
+        if (((holst.length >= 1)&&(holst.length<=100))&&((holst[0].length >= 1)&&(holst[0].length<=100))) {
+            if((n<0)||(n>5000)) return -2;
+            int t1=0, t2=0, t3=0, t4=0;
+            for (int i = 0; i < n; i++) {
+                t1 = coords[i][0];
+                t2 = coords[i][1];
+                t3 = coords[i][2];
+                t4 = coords[i][3];
+                if (t1 > t3 || t1 > holst.length || t3 > holst.length || t1 < 0)
+                    return -5;
+                if ((t2 > t4) || (t2 > holst[0].length) || (t4 > holst[0].length) || (t2 < 0) || (t4 < 0))
+                    return -5;
+                for (int j = t1; j < t3; j++) {
+                    for (int k = t2; k < t4; k++) {
+                        holst[j][k] = false;
+                    }
                 }
-                int[] out = findDiagonalOrder(list);
-                for (int j : out) System.out.print(j + " ");
             }
+            for (boolean[] booleans : holst) {
+                for (boolean aBoolean : booleans) {
+                    if (aBoolean) output++;
+                }
+            }
+        }
+        else output = -1;
+        return output;
+    }
+    public static void main(String[] args) {
+        try{
+            Scanner in = new Scanner(file);
+            int w = in.nextInt(), h = in.nextInt();
+            int n = in.nextInt();
+            boolean[][] holst = new boolean[w][h];
+            for(int i = 0; i < w; i++){
+                for(int j = 0; j < h; j++) holst[i][j] = true;
+            }
+            int[][] coords = new int[n][4];
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < 4; j++){
+                    coords[i][j] = in.nextInt();
+                }
+            }
+            in.close();
+
+            FileWriter fileOutputStream = new FileWriter(new File("src/main/java/org/example/text/output.txt"));
+            fileOutputStream.append(String.valueOf(painter(holst, coords, n)));
+            fileOutputStream.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,113 +2,119 @@ package org.example;
 
 import org.junit.jupiter.api.*;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+
 class MainTest {
-    List<List<Integer>> read(int type){
-        List<List<Integer>> out = new ArrayList<>();
-        String str;
-        switch (type) {
-            case 3 -> {
-                try (Scanner in = new Scanner(new File("D:\\Programming\\Java\\unit_test1\\src\\test\\java\\org\\example\\test\\test3.txt"))) {
-                    while (in.hasNextLine()) {
-                        str = in.nextLine();
-                        in.nextLine();
-                        String[] s = str.split(" ");
-                        int[] a = new int[s.length];
-                        for (int i = 0; i < s.length; i++)
-                            a[i] = Integer.parseInt(s[i]);
-                        out.add(Arrays.stream(a).boxed().toList());
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-            case 4 -> {
-                try (Scanner in = new Scanner(new File("D:\\Programming\\Java\\unit_test1\\src\\test\\java\\org\\example\\test\\test4.txt"))) {
-                    while (in.hasNextLine()) {
-                        str = in.nextLine();
-                        String[] s = str.split(" ");
-                        int[] a = new int[s.length];
-                        for (int i = 0; i < s.length; i++) a[i] = Integer.parseInt(s[i]);
-                        out.add(Arrays.stream(a).boxed().toList());
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return out;
-}
-
     @Nested
-    @DisplayName("Diagonal")
-    class DiagonalTest {
+    @DisplayName("Input data validation")
+    class InputTest{
         @Test
-        void test1() {
-            System.out.println("Test 1 in process");
-            List<List<Integer>> list = new ArrayList<>();
-            int[] a = new int[]{1, 2, 3};
-            list.add(Arrays.stream(a).boxed().toList());
-            a = new int[]{4, 5, 6};
-            list.add(Arrays.stream(a).boxed().toList());
-            a = new int[]{7, 8, 9};
-            list.add(Arrays.stream(a).boxed().toList());
-            int[] test = new int[]{1, 4, 2, 7, 5, 3, 8, 6, 9};
-            Assertions.assertArrayEquals(test, Main.findDiagonalOrder(list));
+        void holst(){
+            try(Scanner in = new Scanner(new File("src/test/java/org/example/test/testholst.txt"))){
+                int w = in.nextInt(), h = in.nextInt();
+                int n = in.nextInt();
+                Assertions.assertTrue(((1 <= w)&&(w <= 100))&&((1 <= h)&&(h <= 100)));
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
         }
-
         @Test
-        void test2() {
-            System.out.println("Test 2 in process");
-            List<List<Integer>> list = new ArrayList<>();
-            int[] a = new int[]{1, 2, 3, 4, 5};
-            list.add(Arrays.stream(a).boxed().toList());
-            a = new int[]{6, 7};
-            list.add(Arrays.stream(a).boxed().toList());
-            a = new int[]{8};
-            list.add(Arrays.stream(a).boxed().toList());
-            a = new int[]{9, 10, 11};
-            list.add(Arrays.stream(a).boxed().toList());
-            a = new int[]{12, 13, 14, 15, 16};
-            list.add(Arrays.stream(a).boxed().toList());
-            int[] test = new int[]{1, 6, 2, 8, 7, 3, 9, 4, 12, 10, 5, 13, 11, 14, 15, 16};
-            Assertions.assertArrayEquals(test, Main.findDiagonalOrder(list));
+        void num(){
+            try(Scanner in = new Scanner(new File("src/test/java/org/example/test/numtest.txt"))){
+                int w = in.nextInt(), h = in.nextInt();
+                int n = in.nextInt();
+                Assertions.assertTrue((n>=0)&&(n<=5000));
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
         }
-
         @Test
-        void test3() {
-            System.out.println("Test 3 in process");
-            int[] accept = new int[]{1, 2, 3, 4, 5, 6};
-            List<List<Integer>> list = read(3);
-            Assertions.assertArrayEquals(accept, Main.findDiagonalOrder(list));
-        }
-
-        @Test
-        void test4() {
-            System.out.println("Test 4 in process");
-            int[] accept = new int[]{14, 13, 12, 11, 14, 19, 13, 15, 16, 1, 8, 9, 11};
-            List<List<Integer>> list = read(4);
-            Assertions.assertArrayEquals(accept, Main.findDiagonalOrder(list));
+        void coords() {
+            try (Scanner in = new Scanner(new File("src/test/java/org/example/test/coordstest.txt"))) {
+                int w = in.nextInt(), h = in.nextInt();
+                int n = in.nextInt();
+                boolean[][] holst = new boolean[w][h];
+                for (int i = 0; i < w; i++) {
+                    for (int j = 0; j < h; j++) holst[i][j] = true;
+                }
+                int[][] coords = new int[n][4];
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        coords[i][j] = in.nextInt();
+                    }
+                }
+                Assertions.assertNotEquals(-5, Main.painter(holst, coords, n));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
     @Nested
-    @DisplayName("Min Num")
-    class PairTest {
+    @DisplayName("Tests")
+    class ProgramTest{
         @Test
-        void test1() {
-            int[] nums = new int[]{3, 5, 2, 3};
-            Assertions.assertEquals(7, Main.minPairSum(nums));
+        void test1(){
+            try(Scanner in = new Scanner(new File("src/test/java/org/example/test/test1.txt"))){
+                int w = in.nextInt(), h = in.nextInt();
+                int n = in.nextInt();
+                boolean[][] holst = new boolean[w][h];
+                for(int i = 0; i < w; i++){
+                    for(int j = 0; j < h; j++) holst[i][j] = true;
+                }
+                int[][] coords = new int[n][4];
+                for(int i = 0; i < n; i++){
+                    for(int j = 0; j < 4; j++){
+                        coords[i][j] = in.nextInt();
+                    }
+                }
+                Assertions.assertEquals(18,Main.painter(holst, coords, n));
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
         }
-
         @Test
-        void test2() {
-            int[] nums = new int[]{3, 5, 4, 2, 4, 6};
-            Assertions.assertEquals(8, Main.minPairSum(nums));
+        void test2(){
+            try(Scanner in = new Scanner(new File("src/test/java/org/example/test/test2.txt"))){
+                int w = in.nextInt(), h = in.nextInt();
+                int n = in.nextInt();
+                boolean[][] holst = new boolean[w][h];
+                for(int i = 0; i < w; i++){
+                    for(int j = 0; j < h; j++) holst[i][j] = true;
+                }
+                int[][] coords = new int[n][4];
+                for(int i = 0; i < n; i++){
+                    for(int j = 0; j < 4; j++){
+                        coords[i][j] = in.nextInt();
+                    }
+                }
+                Assertions.assertEquals(17,Main.painter(holst, coords, n));
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+        @Test
+        void test3(){
+            try(Scanner in = new Scanner(new File("src/test/java/org/example/test/test3.txt"))){
+                int w = in.nextInt(), h = in.nextInt();
+                int n = in.nextInt();
+                boolean[][] holst = new boolean[w][h];
+                for(int i = 0; i < w; i++){
+                    for(int j = 0; j < h; j++) holst[i][j] = true;
+                }
+                int[][] coords = new int[n][4];
+                for(int i = 0; i < n; i++){
+                    for(int j = 0; j < 4; j++){
+                        coords[i][j] = in.nextInt();
+                    }
+                }
+                Assertions.assertEquals(1,Main.painter(holst, coords, n));
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
         }
     }
+
 }
